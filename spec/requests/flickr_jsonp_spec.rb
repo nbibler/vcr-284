19920e90e7704d1f9a165a3931d930212e407a65
Nathaniel Bibler <git@nathanielbibler.com>
2013-04-13 23:55:59 -0400

    Annotate the specs.

--- spec/requests/flickr_jsonp_spec.rb
+++ spec/requests/flickr_jsonp_spec.rb
@@ -3,14 +3,18 @@ require 'spec_helper'
 describe FlickrJsonp do
   describe '#callback_method' do
     it 'returns the callback requested', vcr: { tag: :flickr } do
-      FlickrJsonp.new.callback_method('testmethod').
-        should == 'testmethod'
+      puts "Requesting for 'testmethod' callback value"
+      callback = FlickrJsonp.new.callback_method('testmethod')
+      puts "Received #{callback.inspect} method."
+      callback.should == 'testmethod'
     end
 
     it 'returns a random callback method', vcr: { tag: :flickr, match_requests_on: [:method, VCR.request_matchers.uri_without_params(:jsoncallback)] } do
       random_method = "test#{SecureRandom.hex}"
-      FlickrJsonp.new.callback_method(random_method).
-        should == random_method
+      puts "Requesting for #{random_method.inspect} callback value"
+      callback = FlickrJsonp.new.callback_method(random_method)
+      puts "Received #{callback.inspect} method."
+      callback.should == random_method
     end
   end
 end
